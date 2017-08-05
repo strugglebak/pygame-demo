a2033612623cdc48ca1ff6df77467419f22747f4
xm <dxp157@sohu.com>
2017-08-05 23:56:51 +0800

    主要实现了子弹类的重新开始的方法，重构了move方法，添加了active这一变量，利用这个变量来判定子弹是否出界

--- bullet.py
+++ bullet.py
@@ -5,10 +5,20 @@ class Bullet:
 		self.x = 0
 		self.y = 0 - 1
 		self.image = pygame.image.load(img_name).convert_alpha()
-		
-	def move(self, mouse_x, mouse_y, speed):
+		self.speed = 3
+		self.active = False
+
+	def restart(self):
+		(mouse_x, mouse_y) = pygame.mouse.get_pos()
+		self.x = mouse_x - self.image.get_width() / 2
+		self.y = mouse_y - self.image.get_height() / 2
+		self.active = True
+
+	def move(self):
+		if self.active:
+			self.y -= self.speed
+
 		if self.y < 0:
-			self.x = mouse_x - self.image.get_width() / 2
-			self.y = mouse_y - self.image.get_height() / 2	
-		else:
-			self.y -= speed
+			self.active = False
+			
+			
